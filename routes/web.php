f4534b44dc624778c6b833b734ca6bf56cdd5296
Maria Barraza <reikafer@gmail.com>
2020-04-27 22:10:10 -0700

    terminado

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::resource('/admin/estufasReparar', 'Admin\EstufasReparacionesController');
 
 Route::resource('/admin/usuarios', 'Admin\UsuarioController');
 
+Auth::routes('register' ==  false);
 
 Route::get('/search', 'Admin\EstufasController@search');
 Route::get('/search', 'Admin\EstufasReparacionesController@search');
